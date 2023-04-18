68e6f3f2c060fb12a2e1ee5189ba40d81b4a8f4f
Ajarek <ajarek@poczta.onet.pl>
2023-04-18 21:55:05 +0200

    change images on data.json

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,15 +1,24 @@
 import React, { useEffect, useState, useContext } from 'react'
+import data from '../../assets/data.json'
 
 import './Dashboard.css'
 
 
 const Dashboard = () => {
- 
+ console.log(data);
   
   return (
     <div className='dashboard'>
-    <h1>Dashboard </h1>
-    <img src="/Images/medium (1).webp" alt="" />
+    
+    {data.map((dt)=>{
+      return(
+        <div className="wrapper" key={dt.id}>
+          <img src={dt.image} alt="" width={'100%'} />
+
+        </div>
+      )
+
+    })}
     </div>
   )
 }
